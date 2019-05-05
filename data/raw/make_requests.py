2cfb9f604ad234bd7760f843856b0f81a2d4d585
zyxue <alfred532008@gmail.com>
2019-05-05 16:19:36 -0700

    updated make_requests.py for multiple neigborhood

--- data/raw/make_requests.py
+++ data/raw/make_requests.py
@@ -1,22 +1,53 @@
 import os
-import requests
+import logging
+import time
 
-years = range(2018, 2008, -1)
+import requests
+import numpy as np
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s|%(levelname)s|%(message)s')
+
+years = range(2019, 2015, -1)
 weeks = range(1, 53)
 
-url_base = 'http://maps.latimes.com/neighborhoods/neighborhood/downtown/crime/by-week'
+url_base = 'http://maps.latimes.com/neighborhoods/neighborhood/'
 
-for year in years:
-    outdir = str(year)
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
 
-    for week in weeks:
-        url = f'{url_base}/{year}/{week:02}'
-        print(url)
-        resp = requests.get(url)
-        print(resp.status_code)
+neighborhoods = []
+with open('./neighborhoods.csv', 'rt') as inf:
+    for line in inf:
+        neighborhoods.append(line.strip())
 
-        out = os.path.join(outdir, f'{week:02}.html')
-        with open(out, 'wt') as opf:
-            opf.write(resp.text)
+for neig in neighborhoods:
+    for year in years:
+        for week in weeks:
+            outdir = f'by_neighborhood/{neig}/{year}/{week}'
+            out = os.path.join(outdir, f'{week:02}.html')
+            if os.path.exists(out):
+                logging.warning(f'{out} already exists')
+                continue
+
+            url = f'{url_base}/{neig}/crime/by-week/{year}/{week:02}'
+            logging.info(url)
+
+            try:
+                resp = requests.get(url)
+            except Exception as err:
+                logging.exception(err)
+                continue
+
+            logging.info(resp.status_code)
+            if resp.status_code != 200:
+                continue
+
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)
+
+            logging.info(f'writing {out}')
+            with open(out, 'wt') as opf:
+                opf.write(resp.text)
+
+            time.sleep(np.random.random())
